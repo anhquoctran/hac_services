4aa88a82df7e1ff2566eacbbbedab84c180ff35c
anhquoctran <aquoc.hue@outlook.com>
2018-06-05 17:35:20 +0700

    fix cronJob

--- app/jobs/cleanupJob.js
+++ app/jobs/cleanupJob.js
@@ -4,9 +4,10 @@ var Plate = require('../models/platetbl')(db, sequelize)
 var CronJob = require('cron').CronJob
 var moment = require('moment')
 
-var job = new CronJob('00 00 08 * * 1-7', cleanupPlate, null, false, 'Asia/Ho_Chi_Minh')
+var job = new CronJob('00 00 08 * * 1-7', cleanupPlate, onCompleted, false, 'Asia/Ho_Chi_Minh')
 
 function cleanupPlate() {
+    console.log("Starting cleaner...")
     db.sync()
     .then(() => {
         Plate.destroy({ where: { frametime: { $lte: moment().subtract(7, 'days').toDate() }}})
@@ -14,6 +15,10 @@ function cleanupPlate() {
     .catch(error => {
         console.error(error)
     })
+}
+
+function onCompleted() {
+    console.log("Cleanup completed...")
 }
 
 module.exports = job;
